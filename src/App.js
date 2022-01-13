c8c4192f88ee03f89eb17cd527121d634533574c
jngnandan <jngnandan@gmail.com>
2022-01-13 16:57:49 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css'
 
-export default App;
+const App = () => (
+  <Router>
+    <Routes>
+      <Route exact path="/login" element={<LoginForm />} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/products" element={<Products />} />
+      <Route exact path="/cart" element={<Cart />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Router>
+)
+
+export default App
